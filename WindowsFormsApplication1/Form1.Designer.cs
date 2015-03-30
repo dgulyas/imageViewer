ab91af5d48ceb211692b05d64444c9150bf783c5
David Gulyas <dgulyas@invidi.com>
2015-03-30 13:45:02 -0600

    Initial commit
    Things and stuff

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApplication1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.previousButton = new System.Windows.Forms.Button();
+			this.nextButton = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// previousButton
+			// 
+			this.previousButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.previousButton.Location = new System.Drawing.Point(13, 604);
+			this.previousButton.Name = "previousButton";
+			this.previousButton.Size = new System.Drawing.Size(75, 23);
+			this.previousButton.TabIndex = 1;
+			this.previousButton.Text = "Previous";
+			this.previousButton.UseVisualStyleBackColor = true;
+			this.previousButton.Click += new System.EventHandler(this.previousButton_Click);
+			// 
+			// nextButton
+			// 
+			this.nextButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.nextButton.Location = new System.Drawing.Point(94, 604);
+			this.nextButton.Name = "nextButton";
+			this.nextButton.Size = new System.Drawing.Size(75, 23);
+			this.nextButton.TabIndex = 2;
+			this.nextButton.Text = "Next";
+			this.nextButton.UseVisualStyleBackColor = true;
+			this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScroll = true;
+			this.ClientSize = new System.Drawing.Size(831, 639);
+			this.Controls.Add(this.nextButton);
+			this.Controls.Add(this.previousButton);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button previousButton;
+		private System.Windows.Forms.Button nextButton;
+	}
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+	public partial class Form1 : Form
+	{
+		private int m_currentIndex = 0;
+		private ArrayList m_pics = new ArrayList();
+
+
+		public Form1()
+		{
+			InitializeComponent();
+			FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+			DialogResult result = folderBrowserDialog.ShowDialog();
+
+			
+			string folder = "";
+			if (result == DialogResult.OK) // Test result.
+			{
+				folder = folderBrowserDialog.SelectedPath;
+			}
+			else
+			{
+				return;
+			}
+
+			var files = Directory.GetFiles(folder);
+
+			
+	
+			var ImageExtensions = new List<string> { ".JPG", ".JPE", ".BMP", ".GIF", ".PNG" };
+			foreach (var file in files)
+			{
+				if (ImageExtensions.Contains(Path.GetExtension(file).ToUpperInvariant()))
+				{
+					m_pics.Add(file);
+				}
+			}
+
+			if (m_pics.Count < 1)
+			{
+				return;
+			}
+
+			SetPicture();
+
+
+
+		}
+
+
+
+		private void SetPicture()
+		{
+
+			pictureBox1.Image = Image.FromFile((string)m_pics[m_currentIndex]);
+
+			pictureBox1.Height = pictureBox1.Image.Height;
+			pictureBox1.Width = pictureBox1.Image.Width;
+
+			//Form1. make is scroll to the top
+		}
+
+
+		private void previousButton_Click(object sender, EventArgs e)
+		{
+			if (m_currentIndex > 0)
+			{
+				m_currentIndex--;
+				SetPicture();
+			}
+		}
+
+		private void nextButton_Click(object sender, EventArgs e)
+		{
+			if (m_currentIndex < m_pics.Count - 1)
+			{
+				m_currentIndex++;
+				SetPicture();
+			}
+		}
+
+		
+
+	}
+}
